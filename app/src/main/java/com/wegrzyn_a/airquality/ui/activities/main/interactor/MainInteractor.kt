06a7af1d2d85f139d0d8b05d31347ee3e3b12f32
Andrzej Węgrzyn <andrzej9393@gmail.com>
2018-06-13 23:44:21 +0200

    main interactor improvements

--- app/src/main/java/com/wegrzyn_a/airquality/ui/activities/main/interactor/MainInteractor.kt
+++ app/src/main/java/com/wegrzyn_a/airquality/ui/activities/main/interactor/MainInteractor.kt
@@ -4,18 +4,19 @@ import com.wegrzyn_a.airquality.ui.activities.main.model.MeasurementModel
 import com.wegrzyn_a.airquality.web.RestApi
 import io.reactivex.Single
 
-class MainInteractor: IMainInteractor {
-    override fun getData(): Single<List<MeasurementModel>> {
-        return RestApi.getMeasurement(92)
-                .map {
-                    it.values.mapIndexed { index, dataModel ->
-//                        val millis = DateUtils.parseWebDate(it.date).time
-//                        val millis = 5f
-                        val value = dataModel.value.toFloat()
-                        val label = dataModel.date
-//                        Entry(millis.toFloat(),value)
-                        MeasurementModel(index,value,label)
-                    }
-                }
-    }
+class MainInteractor : IMainInteractor {
+    override fun getData(): Single<List<MeasurementModel>> = getLastN(10, 0.01f)
+
+    private fun getLastN(n: Int, min: Float) = RestApi.getMeasurement(92)
+            .map {
+                it.values
+                        .reversed()
+                        .filterIndexed { index, measurementDataResponse -> index < n }
+                        .filter { it.value > min }
+                        .mapIndexed { index, dataModel ->
+                            val value = dataModel.value.toFloat()
+                            val label = dataModel.date
+                            MeasurementModel(index, value, label)
+                        }
+            }
 }
