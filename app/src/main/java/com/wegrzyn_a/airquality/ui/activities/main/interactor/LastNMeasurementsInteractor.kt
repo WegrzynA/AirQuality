182a7730a48f0d126c52e4202f7fd648401aa59b
Andrzej Węgrzyn <andrzej9393@gmail.com>
2018-06-16 18:42:01 +0200

    refactor

--- app/src/main/java/com/wegrzyn_a/airquality/ui/activities/main/interactor/LastNMeasurementsInteractor.kt
+++ app/src/main/java/com/wegrzyn_a/airquality/ui/activities/main/interactor/LastNMeasurementsInteractor.kt
@@ -2,6 +2,7 @@ package com.wegrzyn_a.airquality.ui.activities.main.interactor
 
 import com.wegrzyn_a.airquality.ui.activities.main.entity.MeasurementEntity
 import com.wegrzyn_a.airquality.ui.activities.main.entity.MeasurementStationEntity
+import com.wegrzyn_a.airquality.ui.activities.main.interactor.exception.NoSensorException
 import com.wegrzyn_a.airquality.ui.utils.DateUtils
 import com.wegrzyn_a.airquality.web.RestApi
 import io.reactivex.Single
--- app/src/main/java/com/wegrzyn_a/airquality/ui/activities/main/interactor/exception/NoSensorException.kt
+++ app/src/main/java/com/wegrzyn_a/airquality/ui/activities/main/interactor/exception/NoSensorException.kt
@@ -1,3 +1,3 @@
-package com.wegrzyn_a.airquality.ui.activities.main.interactor
+package com.wegrzyn_a.airquality.ui.activities.main.interactor.exception
 
 class NoSensorException: Exception("Sensor not found for selected station")
